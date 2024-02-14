e2739fc54b3afa5716f9d6976d008de36ff15b9e
Qinan Gao <qinanagao@gmail.com>
2024-02-14 22:10:45 +0000

    add props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import TipCalculator from "./Component/TipCalculator";
 import Result from "./Component/Result";
+import { useState } from "react";
 
 function App() {
+  const [bill, setBill] = useState("");
+  const [selectTip, setSelectTip] = useState("");
+  const [numberOfPpl, setNumberOfPpl] = useState("");
   return (
     <div>
       <h1>SPLITTER</h1>
       <div className="App">
-        <TipCalculator />
+        <TipCalculator
+          bill={bill}
+          selectTip={selectTip}
+          numberOfPpl={numberOfPpl}
+          onSetBill={setBill}
+          onSetNumberOfPpl={setNumberOfPpl}
+        />
         <Result />
       </div>
     </div>
